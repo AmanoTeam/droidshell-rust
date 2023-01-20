d2f0ec1b938e74622d7413db03ece55b36673e78
BRENER <brenerviotto@gmail.com>
2023-01-20 15:21:57 -0300

    hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use teloxide::prelude::*;
+
+#[tokio::main]
+async fn main() {
+    run(Environment::new("TELEGRAM_BOT_TOKEN")).await;
+}
+
+#[teloxide(command("start"))]
+async fn start(cx: CommandCx<Message>) -> TeloxideResult<()> {
+    cx.answer("Hello, world!").send().await?;
+    Ok(())
 }
